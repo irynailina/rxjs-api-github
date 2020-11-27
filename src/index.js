bd72580e6b0cb04acd2ae086f847fe885a71a02b
irynailina <irynailina@gmail.com>
2020-11-27 17:09:05 +0200

    ajax with rxjs operators

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import { EMPTY, fromEvent } from "rxjs";
+import {
+  map,
+  debounceTime,
+  distinctUntilChanged,
+  switchMap,
+  mergeMap,
+  tap,
+  catchError,
+  filter
+} from "rxjs/operators";
+import { ajax } from "rxjs/ajax";
+
+const url = "https://api.github.com/search/users?q=";
+
+const search = document.getElementById("search");
+const result = document.getElementById("result");
+const stream$ = fromEvent(search, "input").pipe(
+  map((e) => e.target.value),
+  debounceTime(1000),
+  distinctUntilChanged(),
+  tap(() => result.innerHTML = ''),
+  filter(v => v.trim()),
+  switchMap(v => ajax.getJSON(url + v).pipe(
+      catchError(error => EMPTY)
+  )),
+  map((response) => response.items),
+  mergeMap((items) => items)
+);
+
+stream$.subscribe((user) => {
+  // console.log(val)
+  const html = ` <div class="card">
+    <div class="card-image">
+      <img src="${user.avatar_url}"/>
+      <span class="title">${user.login}</span>
+    </div>
+    <div class="card-action">
+      <a href="${user.html_url}" target="_blank">Открыть GitHub</a>
+    </div>
+  </div>`;
+  result.insertAdjacentHTML("beforeend", html);
+});
